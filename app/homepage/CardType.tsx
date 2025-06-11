86a9a8cf8ff8eb2493587c394424de9b595f4313
Andrew Marcuse <amarcuse@cardconnect.com>
2025-06-11 10:28:08 -0400

    Card type translations

--- app/homepage/CardType.tsx
+++ app/homepage/CardType.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router";
 
 type CardType = {
-    name: string;
+    key: string;
     prefix: RegExp;
     logo: string;
     samplePrefixes: string[];
@@ -11,12 +11,13 @@ type CardType = {
 
 
 const cardtypes = [
-    { name: "Visa", prefix: new RegExp("^4"), logo: "/images/visa.svg", samplePrefixes: ["4"]},
-    { name: "MasterCard", prefix: RegExp("^5[1-5]"), logo: "/images/mastercard.svg",  samplePrefixes: ["51", "52", "53"] },
-    { name: "American Express", prefix: RegExp("^3[47]"), logo: "/images/amex.svg", samplePrefixes: ["34", "37"] },
-    { name: "Discover", prefix: RegExp("^6(011|5)"), logo: "/images/discover.svg",  samplePrefixes: ["6011", "65"] },
-    { name: "Diners Club", prefix: RegExp("^3[068]"), logo: "/images/dinersclub.svg",  samplePrefixes: ["30", "36", "38"] },
-    { name: "JCB", prefix: RegExp("^35"), logo: "/images/jcb.svg",  samplePrefixes: ["35"] },
+    { key: "card_visa", prefix: new RegExp("^4"), logo: "/images/visa.svg", samplePrefixes: ["4"]},
+    { key: "card_mastercard", prefix: RegExp("^5[1-5]"), logo: "/images/mastercard.svg",  samplePrefixes: ["51", "52", "53"] },
+    { key: "card_amex", prefix: RegExp("^3[47]"), logo: "/images/amex.svg", samplePrefixes: ["34", "37"] },
+    { key: "card_discover", prefix: RegExp("^6(011|5)"), logo: "/images/discover.svg",  samplePrefixes: ["6011", "65"] },
+    { key: "card_diners", prefix: RegExp("^3[068]"), logo: "/images/dinersclub.svg",  samplePrefixes: ["30", "36", "38"] },
+    { key: "card_jcb", prefix: RegExp("^35"), logo: "/images/jcb.svg",  samplePrefixes: ["35"] },
+    { key: "card_unionpay", prefix: RegExp("^62"), logo: "/images/unionpay.svg",  samplePrefixes: ["62"] },
 ];
 
 export function getCardType(number: string | null): CardType | null {
@@ -37,7 +38,7 @@ export function CardType({ target, clickable }: { target: string | null, clickab
     const [searchParams, setSearchParams] = useSearchParams();
 
     const logo = cardType ? cardType.logo : "/images/unknown.svg";
-    const text = cardType ? cardType.name : t("card_unknown");
+    const text = t(cardType ? cardType.key : "card_unknown");
     const clickFn = clickable && cardType ? () => {
         const prefixes = cardType.samplePrefixes;
         const prefix = prefixes.length > 0 ? prefixes[Math.floor(Math.random() * prefixes.length)] : "";
